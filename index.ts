07ebee3beabd52229acb88da3748e15eaf9b8b69
Enzo Avagliano <eavagliano@laprovence.com>
2024-01-17 15:21:39 +0100

    Added start script with typescript compilation + added comments

--- index.ts
+++ index.ts
@@ -1,14 +1,17 @@
 const input = require("prompt-sync")({ sigint: true });
 
-const user_number = input("Entrez un nombre :  ");
+const user_number: number = parseInt(input("Entrez un nombre : "));
 
 // Création d'une fonction fizzbuzz qui prends en paramètre le nombre choisi par l'utilisateur
 // Ici on utilise l'operateur modulo "%" qui permet de récupérer le reste d'une division
 // Si le reste est egal à 0, alors le nombre est divisible par le nombre choisi
-function fizzbuzz(number) {
+function fizzbuzz(number: number) {
+  // On vérifie que la saisie de l'utilisateur est bien un nombre
+  if (isNaN(number)) return console.log("Votre saisie est invalide...");
+
   // On boucle jusqu'au nombre choisi par l'utilisateur
-  let i = 1; // On demare la boucle à 1
-  while (i <= user_number) {
+  let i: number = 1; // On demare la boucle à 1
+  while (i <= number) {
     // REGLE 1 : "if number can be divided by 3 AND 5 : display FizzBuzz;"
     if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
 
